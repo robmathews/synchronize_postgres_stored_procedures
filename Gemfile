42520252ebadb05831b179d620416d162e829fbb
rob mathews <rob.mathews@goodmeasures.com>
2024-07-10 12:12:09 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in synchronize_postgres_stored_procedures.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
+
+gem "rspec", "~> 3.0"
---
+++ lib/synchronize_postgres_stored_procedures.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "synchronize_postgres_stored_procedures/version"
+
+module SynchronizePostgresStoredProcedures
+  class Error < StandardError; end
+  # Your code goes here...
+end
